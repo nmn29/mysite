08b0fcf23eda6585c58b7673d9545a5e94afb039
nmn29 <ina010218@gmail.com>
2022-05-12 14:57:07 +0900

    a

--- src/top/components/contents.js
+++ src/top/components/contents.js
@@ -1,35 +1,33 @@
 import "../stylesheet/contents.css";
-import {Fade} from 'react-reveal'
+import { Fade } from 'react-reveal'
 import Icon from "../images/icon.png"
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 
 export default function contents(){ 
   return(
     <div className="contents">
       <div className="about-fadeup">
         <div className="about">
-          <ul>
+          <ul className="aboutul">
             <li className="icon-l">
             <div className="icon">
-              <motion.div
-                whileHover={{
-                  scale: 1.1
-                }}
-                whileTap={{ scale: 0.9 }}
-              >
+              <motion.div whileHover={{scale: 1.1}} whileTap={{ scale: 0.9 }}>
                 <a href="/"><img className="icon-c" src={Icon} /></a>
               </motion.div>
             </div>
             </li>
             <li>
-                <div className="aboutus">
-                  <Fade bottom><h1>about us</h1></Fade>                 
-                  <Fade bottom><p>namaです</p></Fade>                 
-                  <Fade bottom><p>駆け出しエンジニアの学生です</p></Fade>                 
-                  <Fade bottom><p>よろしくお願いします</p></Fade>                 
-                </div>
+              <div className="aboutus">
+                <Fade bottom><h1>about us</h1></Fade>                 
+                <Fade bottom><p>namaです</p></Fade>                 
+                <Fade bottom><p>駆け出しエンジニアの学生です</p></Fade>                 
+                <Fade bottom><p>よろしくお願いします</p></Fade>                 
+              </div>
             </li>
-            </ul>      
+          </ul>
+            <div className="skills">
+              <Fade bottom><h1>skills</h1></Fade>  
+            </div> 
         </div>
       </div>
     </div>
--- src/top/components/header.js
+++ src/top/components/header.js
@@ -1,18 +1,25 @@
 import "../stylesheet/header.css"
 import Icon from "../images/icon.png"
 import { motion } from "framer-motion"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithubSquare, faTwitterSquare } from "@fortawesome/free-brands-svg-icons";
 
 export default function header(){
+
+
   return(
     <header>
-      <motion.div
-        whileHover={{
-          scale: 1.1
-        }}
-        whileTap={{ scale: 0.9 }}
-      >
+      <motion.div whileHover={{scale: 1.1}} whileTap={{ scale: 0.9 }}>
         <a href="/"><img className="icon-h" src={Icon} /></a>
       </motion.div>
+      <ul className="link-l">
+        <li className="g-link">
+          <a href="https://github.com/nmn29"><FontAwesomeIcon icon={faGithubSquare} /></a>
+        </li>
+        <li className="t-link">
+          <a href="https://github.com/nmn29"><FontAwesomeIcon icon={faTwitterSquare} /></a>
+        </li>
+      </ul>
     </header>
   );
 }
